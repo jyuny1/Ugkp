be56f0695a800a488ccc211b10298a93c8327203
Jyun-Yi Liou <7yuny1@gmail.com>
2010-02-14 04:34:54 +0800

    bump toc version to 30300 fix some issues for patch 3.3

--- Ugkp-set-item-zhTW.lua
+++ Ugkp-set-item-zhTW.lua
@@ -1,471 +1,428 @@
-Ugkp_item_auction  = {
-  [1] = {
-    ["typename"] = "甲蟲",
-    ["itemnames"] = {
-      [1] = {
-	["itemname"] = "水晶甲蟲",
-	["checked"] = 1
-      },
-      [2] = {
-	["itemname"] = "象牙甲蟲",
-	["checked"] = 1
-      },
-      [3] = {
-	["itemname"] = "黃金甲蟲",
-	["checked"] = 1
-      },
-      [4] = {
-	["itemname"] = "陶土甲蟲",
-	["checked"] = 1
-      },
-      [5] = {
-	["itemname"] = "銀質甲蟲",
-	["checked"] = 1
-      },
-      [6] = {
-	["itemname"] = "岩石甲蟲",
-	["checked"] = 1
-      },
-      [7] = {
-	["itemname"] = "白骨甲蟲",
-	["checked"] = 1
-      },
-      [8] = {
-	["itemname"] = "青銅甲蟲",
-	["checked"] = 1
-      }
-    },
-  },
-  [2] = {
-    ["typename"] = "雕像",
-    ["itemnames"] = {
-      [1] = {
-	["itemname"] = "黑曜石雕像",
-	["checked"] = 1
-      },
-      [2] = {
-	["itemname"] = "朱紅雕像",
-	["checked"] = 1
-      },
-      [3] = {
-	["itemname"] = "柔光雕像",
-	["checked"] = 1
-      },
-      [4] = {
-	["itemname"] = "瑪瑙雕像",
-	["checked"] = 1
-      },
-      [5] = {
-	["itemname"] = "碧藍雕像",
-	["checked"] = 1
-      },
-      [6] = {
-	["itemname"] = "雪白雕像",
-	["checked"] = 1
-      },
-      [7] = {
-	["itemname"] = "琥珀雕像",
-	["checked"] = 1
-      },
-      [8] = {
-	["itemname"] = "翠玉雕像",
-	["checked"] = 1
-      },
-      [9] = {
-	["itemname"] = "死亡雕像",
-	["checked"] = 1
-      },
-      [10] = {
-	["itemname"] = "生命雕像",
-	["checked"] = 1
-      },
-      [11] = {
-	["itemname"] = "太陽雕像",
-	["checked"] = 1
-      },
-      [12] = {
-	["itemname"] = "戰爭雕像",
-	["checked"] = 1
-      },
-      [13] = {
-	["itemname"] = "先知雕像",
-	["checked"] = 1
-      },
-      [14] = {
-	["itemname"] = "征戰雕像",
-	["checked"] = 1
-      },
-      [15] = {
-	["itemname"] = "復生雕像",
-	["checked"] = 1
-      },
-      [16] = {
-	["itemname"] = "夜晚雕像",
-	["checked"] = 1
-      }
-    },
-  },
-  [3] = {
-    ["typename"] = "書籍",
-    ["itemnames"] = {
-      [1] = {
-	["itemname"] = "秘典：奧術飛彈 VIII",
-	["checked"] = 1
-      },
-      [2] = {
-	["itemname"] = "秘典：寒冰箭 XI",
-	["checked"] = 1
-      },
-      [3] = {
-	["itemname"] = "秘典：火球術 XII",
-	["checked"] = 1
-      },
-      [4] = {
-	["itemname"] = "聖典：治療禱言 V",
-	["checked"] = 1
-      },
-      [5] = {
-	["itemname"] = "聖典：強效治療術 V",
-	["checked"] = 1
-      },
-      [6] = {
-	["itemname"] = "聖典：恢復 X",
-	["checked"] = 1
-      },
-      [7] = {
-	["itemname"] = "教程：戰鬥怒吼 VII",
-	["checked"] = 1
-      },
-      [8] = {
-	["itemname"] = "教程：復仇 VI",
-	["checked"] = 1
-      },
-      [9] = {
-	["itemname"] = "教程：英勇打擊 IX",
-	["checked"] = 1
-      },
-      [10] = {
-	["itemname"] = "石板：治療波 X",
-	["checked"] = 1
-      },
-      [11] = {
-	["itemname"] = "石板：風之優雅圖騰 III",
-	["checked"] = 1
-      },
-      [12] = {
-	["itemname"] = "石板：大地之力圖騰 V",
-	["checked"] = 1
-      },
-      [13] = {
-	["itemname"] = "指南：雄鷹守護 VII",
-	["checked"] = 1
-      },
-      [14] = {
-	["itemname"] = "指南：多重射擊 V",
-	["checked"] = 1
-      },
-      [15] = {
-	["itemname"] = "指南：毒蛇釘刺 IX",
-	["checked"] = 1
-      },
-      [16] = {
-	["itemname"] = "手冊：佯攻 V",
-	["checked"] = 1
-      },
-      [17] = {
-	["itemname"] = "手冊：背刺 IX",
-	["checked"] = 1
-      },
-      [18] = {
-	["itemname"] = "手冊：致命毒藥 V",
-	["checked"] = 1
-      },
-      [19] = {
-	["itemname"] = "書卷：回春術 XI",
-	["checked"] = 1
-      },
-      [20] = {
-	["itemname"] = "書卷：星火術 VII",
-	["checked"] = 1
-      },
-      [21] = {
-	["itemname"] = "書卷：治療之觸 XI",
-	["checked"] = 1
-      },
-      [22] = {
-	["itemname"] = "魔典：腐蝕術 VII",
-	["checked"] = 1
-      },
-      [23] = {
-	["itemname"] = "魔典：獻祭 VIII",
-	["checked"] = 1
-      },
-      [24] = {
-	["itemname"] = "魔典：暗影箭 X",
-	["checked"] = 1
-      }
+﻿Ugkp_item_auction  = {
+	[1] = {
+		["typename"] = "寶珠",
+		["itemnames"] = {
+			[1] = {
+				["itemname"] = "太陽微粒",
+				["checked"] = 1
+				},
+			[2] = {
+				["itemname"] = "黑暗之心",
+				["checked"] = 1
+				},
+			[3] = {
+				["itemname"] = "虛空漩渦",
+				["checked"] = 1
+				},
+			[4] = {
+				["itemname"] = "冰凍寶珠",
+				["checked"] = 1
+				},
+			[5] = {
+				["itemname"] = "符文寶珠",
+				["checked"] = 1
+				},
+			[6] = {
+				["itemname"] = "十字軍寶珠",
+				["checked"] = 1
+				},
+				 },
+		},
+	[2] = {
+		["typename"] = "材料",
+		["itemnames"] = {
+			[1] = {
+				["itemname"] = "聯結水晶",
+				["checked"] = 1
+				},
+			[2] = {
+				["itemname"] = "小塊稜石裂片",
+				["checked"] = 1
+				},
+			[3] = {
+				["itemname"] = "大塊稜石裂片",
+				["checked"] = 1
+				},
+			[4] = {
+				["itemname"] = "次級異界精華",
+				["checked"] = 1
+				},
+			[5] = {
+				["itemname"] = "強效異界精華",
+				["checked"] = 1
+				},
+			[6] = {
+				["itemname"] = "虛無水晶",
+				["checked"] = 1
+				},
+			[7] = {
+				["itemname"] = "魔塵",
+				["checked"] = 1
+				},
+			[8] = {
+				["itemname"] = "無限之塵",
+				["checked"] = 1
+				},
+			[9] = {
+				["itemname"] = "次級宇宙精華",
+				["checked"] = 1
+				},
+			[10] = {
+				["itemname"] = "強效宇宙精華",
+				["checked"] = 1
+				},
+			[11] = {
+				["itemname"] = "小塊夢境裂片",
+				["checked"] = 1
+				},
+			[12] = {
+				["itemname"] = "夢境裂片",
+				["checked"] = 1
+				},
+			[13] = {
+				["itemname"] = "深淵水晶",
+				["checked"] = 1
+				},
+			[14] = {
+				["itemname"] = "伊利達瑞的印記",
+				["checked"] = 1
+				},
+			[15] = {
+				["itemname"] = "寒地毛皮",
+				["checked"] = 1
+				},
+			[16] = {
+				["itemname"] = "原始虛空",
+				["checked"] = 1
+				}
 
-    },
-  },
-  [4] = {
-    ["typename"] = "寶石",
-    ["itemnames"] = {
-      [1] = {
-	["itemname"] = "藍色哈卡萊寶石",
-	["checked"] = 1
-      },
-      [2] = {
-	["itemname"] = "紅色哈卡萊寶石",
-	["checked"] = 1
-      },
-      [3] = {
-	["itemname"] = "金色哈卡萊寶石",
-	["checked"] = 1
-      },
-      [4] = {
-	["itemname"] = "紫色哈卡萊寶石",
-	["checked"] = 1
-      },
-      [5] = {
-	["itemname"] = "綠色哈卡萊寶石",
-	["checked"] = 1
-      },
-      [6] = {
-	["itemname"] = "黃色哈卡萊寶石",
-	["checked"] = 1
-      },
-      [7] = {
-	["itemname"] = "橙色哈卡萊寶石",
-	["checked"] = 1
-      },
-      [8] = {
-	["itemname"] = "青銅哈卡萊寶石",
-	["checked"] = 1
-      },
-      [9] = {
-	["itemname"] = "銀色哈卡萊寶石",
-	["checked"] = 1
-      }
-    },
-  },
-  [5] = {
-    ["typename"] = "硬幣",
-    ["itemnames"] = {
+				 },
+		},
+	[3] = {
+		["typename"] = "甲蟲",
+		["itemnames"] = {
+			[1] = {
+				["itemname"] = "水晶甲蟲",
+				["checked"] = 1
+				},
+			[2] = {
+				["itemname"] = "象牙甲蟲",
+				["checked"] = 1
+				},
+			[3] = {
+				["itemname"] = "黃金甲蟲",
+				["checked"] = 1
+				},
+			[4] = {
+				["itemname"] = "陶土甲蟲",
+				["checked"] = 1
+				},
+			[5] = {
+				["itemname"] = "銀質甲蟲",
+				["checked"] = 1
+				},
+			[6] = {
+				["itemname"] = "巖石甲蟲",
+				["checked"] = 1
+				},
+			[7] = {
+				["itemname"] = "白骨甲蟲",
+				["checked"] = 1
+				},
+			[8] = {
+				["itemname"] = "青銅甲蟲",
+				["checked"] = 1
+				}
 
-      [1] = {
-	["itemname"] = "古拉巴什硬幣",
-	["checked"] = 1
-      },
-      [2] = {
-	["itemname"] = "哈卡萊硬幣",
-	["checked"] = 1
-      },
-      [3] = {
-	["itemname"] = "拉扎什硬幣",
-	["checked"] = 1
-      },
-      [4] = {
-	["itemname"] = "沙怒硬幣",
-	["checked"] = 1
-      },
-      [5] = {
-	["itemname"] = "碎顱硬幣",
-	["checked"] = 1
-      },
-      [6] = {
-	["itemname"] = "祖利安硬幣",
-	["checked"] = 1
-      },
-      [7] = {
-	["itemname"] = "邪枝硬幣",
-	["checked"] = 1
-      },
-      [8] = {
-	["itemname"] = "枯木硬幣",
-	["checked"] = 1
-      },
-      [9] = {
-	["itemname"] = "血頂硬幣",
-	["checked"] = 1
-      }
-    },
-  },
-  [6] = {
-    ["typename"] = "其它",
-    ["itemnames"] = {
-      [1] = {
-	["itemname"] = "奧妮克希亞鱗片",
-	["checked"] = 1
-      },
-      [2] = {
-	["itemname"] = "沙漏",
-	["checked"] = 1
-      },
-      [3] = {
-	["itemname"] = "源質礦石",
-	["checked"] = 1
-      },
-      [4] = {
-	["itemname"] = "小塊黑曜石碎片",
-	["checked"] = 1
-      },
-      [5] = {
-	["itemname"] = "大塊黑曜石碎片",
-	["checked"] = 1
-      },
-      [6] = {
-	["itemname"] = "大塊魔光碎片",
-	["checked"] = 1
-      },
-      [7] = {
-	["itemname"] = "強效不滅",
-	["checked"] = 1
-      },
-      [8] = {
-	["itemname"] = "幻影之塵",
-	["checked"] = 1
-      },
-      [9] = {
-	["itemname"] = "夢境之塵",
-	["checked"] = 1
-      },
-      [10] = {
-	["itemname"] = "聖甲蟲箱的鑰匙",
-	["checked"] = 1
-      },
-      [11] = {
-	["itemname"] = "熾熱之核",
-	["checked"] = 1
-      },
-      [12] = {
-	["itemname"] = "熔岩之核",
-	["checked"] = 1
-      },
-      [13] = {
-	["itemname"] = "熔火犬皮",
-	["checked"] = 1
-      },
-      [14] = {
-	["itemname"] = "山脈之血",
-	["checked"] = 1
-      },
-      [15] = {
-	["itemname"] = "大地精華",
-	["checked"] = 1
-      },
-      [16] = {
-	["itemname"] = "火焰精華",
-	["checked"] = 1
-      },
-      [17] = {
-	["itemname"] = "黑鐵礦",
-	["checked"] = 1
-      },
-      [18] = {
-	["itemname"] = "薩弗隆鐵錠",
-	["checked"] = 1
-      },
+				 },
+		},
+	[4] = {
+		["typename"] = "雕像",
+		["itemnames"] = {
+			[1] = {
+				["itemname"] = "黑曜石雕像",
+				["checked"] = 1
+				},
+			[2] = {
+				["itemname"] = "朱紅雕像",
+				["checked"] = 1
+				},
+			[3] = {
+				["itemname"] = "柔光雕像",
+				["checked"] = 1
+				},
+			[4] = {
+				["itemname"] = "瑪瑙雕像",
+				["checked"] = 1
+				},
+			[5] = {
+				["itemname"] = "碧藍雕像",
+				["checked"] = 1
+				},
+			[6] = {
+				["itemname"] = "雪白雕像",
+				["checked"] = 1
+				},
+			[7] = {
+				["itemname"] = "琥珀雕像",
+				["checked"] = 1
+				},
+			[8] = {
+				["itemname"] = "翠玉雕像",
+				["checked"] = 1
+				},
+			[9] = {
+				["itemname"] = "死亡雕像",
+				["checked"] = 1
+				},
+			[10] = {
+				["itemname"] = "生命雕像",
+				["checked"] = 1
+				},
+			[11] = {
+				["itemname"] = "太陽雕像",
+				["checked"] = 1
+				},
+			[12] = {
+				["itemname"] = "戰爭雕像",
+				["checked"] = 1
+				},
+			[13] = {
+				["itemname"] = "先知雕像",
+				["checked"] = 1
+				},
+			[14] = {
+				["itemname"] = "征戰雕像",
+				["checked"] = 1
+				},
+			[15] = {
+				["itemname"] = "復生雕像",
+				["checked"] = 1
+				},
+			[16] = {
+				["itemname"] = "夜晚雕像",
+				["checked"] = 1
+				}
+				 },
+		},
+	[5] = {
+		["typename"] = "寶石",
+		["itemnames"] = {
 
-    },
-  },
-  [7] = {
-    ["typename"] = "塵",
-    ["itemnames"] = {
+			[1] = {
+				["itemname"] = "藍色哈卡萊寶石",
+				["checked"] = 1
+				},
+			[2] = {
+				["itemname"] = "紅色哈卡萊寶石",
+				["checked"] = 1
+				},
+			[3] = {
+				["itemname"] = "金色哈卡萊寶石",
+				["checked"] = 1
+				},
+			[4] = {
+				["itemname"] = "紫色哈卡萊寶石",
+				["checked"] = 1
+				},
+			[5] = {
+				["itemname"] = "綠色哈卡萊寶石",
+				["checked"] = 1
+				},
+			[6] = {
+				["itemname"] = "黃色哈卡萊寶石",
+				["checked"] = 1
+				},
+			[7] = {
+				["itemname"] = "橙色哈卡萊寶石",
+				["checked"] = 1
+				},
+			[8] = {
+				["itemname"] = "青銅哈卡萊寶石",
+				["checked"] = 1
+				},
+			[9] = {
+				["itemname"] = "銀色哈卡萊寶石",
+				["checked"] = 1
+				}
+				 },
+		},
+	[6] = {
+		["typename"] = "硬幣",
+		["itemnames"] = {
 
-      [1] = {
-	["itemname"] = "太陽之塵",
-	["checked"] = 1
-      },
-      [2] = {
-	["itemname"] = "黑暗之塵",
-	["checked"] = 1
-      },
-      [3] = {
-	["itemname"] = "虛空漩渦",
-	["checked"] = 1
-      },
+			[1] = {
+				["itemname"] = "古拉巴什硬幣",
+				["checked"] = 1
+				},
+			[2] = {
+				["itemname"] = "哈卡萊硬幣",
+				["checked"] = 1
+				},
+			[3] = {
+				["itemname"] = "拉紮什硬幣",
+				["checked"] = 1
+				},
+			[4] = {
+				["itemname"] = "沙怒硬幣",
+				["checked"] = 1
+				},
+			[5] = {
+				["itemname"] = "碎顱硬幣",
+				["checked"] = 1
+				},
+			[6] = {
+				["itemname"] = "祖利安硬幣",
+				["checked"] = 1
+				},
+			[7] = {
+				["itemname"] = "邪枝硬幣",
+				["checked"] = 1
+				},
+			[8] = {
+				["itemname"] = "枯木硬幣",
+				["checked"] = 1
+				},
+			[9] = {
+				["itemname"] = "血頂硬幣",
+				["checked"] = 1
+				},
+
+				 },
+		},
+	[7] = {
+		["typename"] = "雜項",
+		["itemnames"] = {
+			[1] = {
+				["itemname"] = "源質礦石",
+				["checked"] = 1
+				},
+			[2] = {
+				["itemname"] = "大塊魔光碎片",
+				["checked"] = 1
+				},
+			[3] = {
+				["itemname"] = "強效不滅精華",
+				["checked"] = 1
+				},
+			[4] = {
+				["itemname"] = "幻影之塵",
+				["checked"] = 1
+				},
+			[5] = {
+				["itemname"] = "夢境之塵",
+				["checked"] = 1
+				},
+			[6] = {
+				["itemname"] = "聖甲蟲箱的鑰匙",
+				["checked"] = 1
+				},
+			[7] = {
+				["itemname"] = "正義寶珠",
+				["checked"] = 1
+				},
+			[8] = {
+				["itemname"] = "山脈之血",
+				["checked"] = 1
+				},
+			[9] = {
+				["itemname"] = "薩弗隆鐵錠",
+				["checked"] = 1
+				},
 
 
-    },
-  },
+				 },
+		},
 }
 
 Ugkp_item_ignore = {
-  [1] = {
-    ["typename"] = "徽章",
-    ["itemnames"] = {
+	[1] = {
+		["typename"] = "徽章",
+		["itemnames"] = {
 
-      [1] = {
-	["itemname"] = "公正徽章",
-	["checked"] = 1
-      },
-      [2] = {
-	["itemname"] = "勇氣徽章",
-	["checked"] = 1
-      },
-      [3] = {
-	["itemname"] = "征服者徽章",
-	["checked"] = 1
-      },
-      [4] = {
-	["itemname"] = "英雄徽章",
-	["checked"] = 1
-      },
-    },
-  },
-  [2] = {
-    ["typename"] = "臨時",
-    ["itemnames"] = {
+			[1] = {
+				["itemname"] = "正義徽章",
+				["checked"] = 1
+				},
+			[2] = {
+				["itemname"] = "英雄紋章",
+				["checked"] = 1
+				},
+			[3] = {
+				["itemname"] = "勇氣紋章",
+				["checked"] = 1
+				},
+			[4] = {
+				["itemname"] = "征服紋章",
+				["checked"] = 1
+				},
+			[5] = {
+				["itemname"] = "石之守衛者裂片",
+				["checked"] = 1
+				},
+			[6] = {
+				["itemname"] = "凱旋紋章",
+				["checked"] = 1
+				},
+			[7] = {
+				["itemname"] = "冰霜紋章",
+				["checked"] = 1
+				},
+				 },
+		},
+	[2] = {
+		["typename"] = "臨時",
+		["itemnames"] = {
 
-      [1] = {
-	["itemname"] = "瓦解法杖",
-	["checked"] = 1
-      },
-      [2] = {
-	["itemname"] = "宇宙灌注者",
-	["checked"] = 1
-      },
-      [3] = {
-	["itemname"] = "遷躍切割者",
-	["checked"] = 1
-      },
-      [4] = {
-	["itemname"] = "毀滅",
-	["checked"] = 1
-      },
-      [5] = {
-	["itemname"] = "相位壁壘",
-	["checked"] = 1
-      },
-      [6] = {
-	["itemname"] = "無盡之刃",
-	["checked"] = 1
-      },
-      [7] = {
-	["itemname"] = "靈弦長弓",
-	["checked"] = 1
-      },
-      [8] = {
-	["itemname"] = "虛空尖刺",
-	["checked"] = 1
-      },
-    },
-  },
+			[1] = {
+				["itemname"] = "瓦解之杖",
+				["checked"] = 1
+				},
+			[2] = {
+				["itemname"] = "宇宙灌溉者",
+				["checked"] = 1
+				},
+			[3] = {
+				["itemname"] = "扭曲切割者",
+				["checked"] = 1
+				},
+			[4] = {
+				["itemname"] = "毀滅",
+				["checked"] = 1
+				},
+			[5] = {
+				["itemname"] = "相位壁壘",
+				["checked"] = 1
+				},
+			[6] = {
+				["itemname"] = "無盡之刃",
+				["checked"] = 1
+				},
+			[7] = {
+				["itemname"] = "虛空之絃長弓",
+				["checked"] = 1
+				},
+			[8] = {
+				["itemname"] = "虛空尖刺",
+				["checked"] = 1
+				},
+				 },
+		},
 }
 function Ugkp_ItemSet_Onload()
 
-  itemtypecount = getn(Ugkp_item_auction);
-  --for i=1,itemtypecount,1 do
-  for i=1,itemtypecount,1 do
-    getglobal("UgkpItemSetBorderFrameTab"..i):SetText(Ugkp_item_auction[i]["typename"])
-  end
+	itemtypecount = getn(Ugkp_item_auction);
+	--for i=1,itemtypecount,1 do
+	for i=1,itemtypecount,1 do
+		getglobal("UgkpItemSetBorderFrameTab"..i):SetText(Ugkp_item_auction[i]["typename"])
+	end
 end
 
 function ugkp_change_item_check()
 
-  if(this:GetChecked()) then
-    Ugkp_item_auction[ugkp_item_selet_tab]["itemnames"][this:GetID()]["checked"]=1
-  else
-    Ugkp_item_auction[ugkp_item_selet_tab]["itemnames"][this:GetID()]["checked"]=0
-  end
+	if(this:GetChecked()) then
+		Ugkp_item_auction[ugkp_item_selet_tab]["itemnames"][this:GetID()]["checked"]=1
+	else
+		Ugkp_item_auction[ugkp_item_selet_tab]["itemnames"][this:GetID()]["checked"]=0
+	end
 end
